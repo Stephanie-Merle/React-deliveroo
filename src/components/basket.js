551e8cba2f8e58ca48d0f261a7fe8f52cd27fd81
Stephanie Merle <merle.stephany@gmail.com>
2019-11-13 17:53:45 +0100

    basket working

--- src/components/basket.js
+++ src/components/basket.js
@@ -5,6 +5,11 @@ import PlusCircle from "./plusCircle";
 const Basket = props => {
   let myOrders = props.myOrders;
   const n = Object.keys(myOrders);
+
+  let subTotal = 0;
+  n.map(el => (subTotal += Number(myOrders[el].total)));
+  console.log(subTotal);
+
   const listing = n.map(el => (
     <div key={myOrders[el].id} className="orders">
       <div onClick={() => props.remove(myOrders[el])}>
@@ -19,9 +24,24 @@ const Basket = props => {
     </div>
   ));
   return (
-    <div className="basket">
+    <div className={subTotal === 0 ? "basket" : "basket green"}>
       <button>Valider mon panier</button>
       {listing}
+      {subTotal === 0 ? null : (
+        <>
+          <hr />
+          <div>
+            Sous-total: <span>{subTotal}</span>
+          </div>
+          <div>
+            Livraison: <span>3.5</span>
+          </div>
+          <hr />
+          <div>
+            Total: <span>{subTotal + Number(3.5)}</span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
